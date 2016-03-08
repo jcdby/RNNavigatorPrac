20f86ebe3343ed489943592e1cbb91c47e698e23
Ethan <jcdby@hotmail.com>
2016-03-08 16:28:09 +0900

    second init

--- index.ios.js
+++ index.ios.js
@@ -16,7 +16,7 @@ class RNNavigatorPrac extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          change something
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
